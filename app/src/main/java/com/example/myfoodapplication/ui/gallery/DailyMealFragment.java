013c1db59e66a68a496cf092961a37110197781f
David Fernando <mahefa.fernando@gmail.com>
2024-03-14 09:46:26 +0000

    Feat(app): add files

---
+++ app/src/main/java/com/example/myfoodapplication/ui/gallery/DailyMealFragment.java
@@ -0,0 +1,31 @@
+package com.example.myfoodapplication.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.myfoodapplication.databinding.DailyMealFragmentBinding;
+
+public class DailyMealFragment extends Fragment {
+
+    private DailyMealFragmentBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = DailyMealFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
